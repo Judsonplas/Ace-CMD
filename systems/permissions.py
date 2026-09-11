f7df8d3a152b3b44b4707523439d716d0d7c190b
Judsonplas <umugussusdude@gmail.com>
2026-09-11 08:39:24 -0400

    Allow Discord administrator permissions

--- systems/permissions.py
+++ systems/permissions.py
@@ -4,20 +4,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
-OWNER_ID = int(
-    os.getenv("BOT_OWNER_ID", 0)
-)
+OWNER_ID = int(os.getenv("BOT_OWNER_ID", 0))
 
 
 def has_role(member, role_id):
     if not role_id:
         return False
-
-    return any(
-        role.id == role_id
-        for role in member.roles
-    )
+    return any(role.id == role_id for role in member.roles)
 
 
 def is_owner(member):
@@ -27,28 +20,14 @@ def is_owner(member):
 def is_admin(member, config):
     return (
         is_owner(member)
-        or has_role(
-            member,
-            config.get("admin_role")
-        )
+        or member.guild_permissions.administrator
+        or has_role(member, config.get("admin_role"))
     )
 
 
 def is_mod(member, config):
-    return (
-        is_admin(member, config)
-        or has_role(
-            member,
-            config.get("mod_role")
-        )
-    )
+    return is_admin(member, config) or has_role(member, config.get("mod_role"))
 
 
 def is_helper(member, config):
-    return (
-        is_mod(member, config)
-        or has_role(
-            member,
-            config.get("helper_role")
-        )
-    )
+    return is_mod(member, config) or has_role(member, config.get("helper_role"))
